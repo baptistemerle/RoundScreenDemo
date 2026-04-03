1891c0a1460cb228d2b952aeea50cf5b174447ac
Baptiste Merle <9810925+baptistemerle@users.noreply.github.com>
2026-04-03 17:20:09 +0200

    Add ability to put multiple callbacks for a value

--- include/observable_value.h
+++ include/observable_value.h
@@ -2,19 +2,32 @@
 #define OBSERVABLE_VALUE_H
 
 #include <functional>
+#include <vector>
 
 template <typename T>
 class ObservableValue
 {
 public:
+    using Callback = std::function<void(const T&)>;
+
     ObservableValue(T initialValue = T{})
       : m_value(initialValue)
     {}
 
-    void attach(std::function<void(const T&)> callback)
+    void attach(Callback callback)
     {
-      m_callback = callback;
+      m_callbacks.push_back(callback);
     }
+
+    /*
+     * Note on observer detachment (detach method):
+     * In this embedded system architecture, observers (such as UI widgets or storage controllers)
+     * are instantiated at boot and live throughout the entire application lifecycle.
+     * Since dynamic subscription/unsubscription is not required, a `detach()` method has been
+     * intentionally omitted. This avoids the overhead and complexity of managing unique IDs,
+     * as `std::function` objects cannot be compared directly in C++.
+     * If needed, we could implement a token system
+     */
 
     operator T() const
     {
@@ -27,9 +40,12 @@ public:
       {
         m_value = newValue;
 
-        if (m_callback)
+        for (const auto& callback : m_callbacks)
         {
-          m_callback(m_value);
+          if (callback)
+          {
+            callback(m_value);
+          }
         }
       }
 
@@ -38,7 +54,7 @@ public:
 
 private:
     T m_value;
-    std::function<void(const T&)> m_callback;
+    std::vector<Callback> m_callbacks;
 };
 
 #endif // OBSERVABLE_VALUE_H
